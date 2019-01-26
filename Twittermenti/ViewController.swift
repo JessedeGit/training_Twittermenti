a33e63e9e46b5ec539a133c2fa8cfe35d032939f
Owner <Owner@Jesses-MacBook-Pro.local>
2019-01-26 12:06:54 +0800

    Initial.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,22 +7,71 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let tweetCount = 100
+    let sentimentClassifier = TwitterSentimentClassifier()
+
+    let swifter = Swifter(consumerKey: "", consumerSecret: "")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-    @IBAction func predictPressed(_ sender: Any) {
     
+    func fetchTweets(){
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText, lang: "en", count: tweetCount, tweetMode: .extended, success: { (results, metadata) in
+                var tweets = [TwitterSentimentClassifierInput]()
+                
+                for i in 0..<self.tweetCount {
+                    if let tweet = results[i]["full_text"].string {
+                        tweets.append( TwitterSentimentClassifierInput(text: tweet))
+                    }
+                }
+                self.makePrediction(tweets: tweets)
+                
+            }) { (error) in
+                print("There was an error with the Twitter API request, \(error)")
+            }
+        }    }
     
+    func makePrediction(tweets: [TwitterSentimentClassifierInput]){
+        do {
+            let predictionResult = try self.sentimentClassifier.predictions(inputs: tweets)
+            
+            var sentimentScore = 0.0
+            for prediction in predictionResult{
+                let setiment = prediction.label
+                if setiment == "Pos" {sentimentScore += 1}
+                else if setiment == "Neg" {sentimentScore -= 1}
+            }
+            print("Score:\(sentimentScore)")
+            
+            updateUI(sentimentScore: sentimentScore)
+            
+        } catch{
+            print("Making predictions Error:\(error)")
+        }
     }
     
+    func updateUI(sentimentScore: Double){
+        if sentimentScore > 20 { self.sentimentLabel.text = "😍"}
+        else if sentimentScore < -20 {self.sentimentLabel.text = "😇"}
+        else  {self.sentimentLabel.text = "🤠"}
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+        
+       fetchTweets()
+    }
 }
 
